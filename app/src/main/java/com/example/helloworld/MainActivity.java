c1077b5859fc90a472bcbca5f68db4c3d1dc77b5
ASUSTUF505GD\ASUS <angger.herlambang@ti.ukdw.ac.id>
2020-09-25 21:47:58 +0700

    commit master

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -12,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.i(TAG, "onCreate : Called");
+        Log.i(TAG, "onCreate : Called 2");
+        Log.i(TAG, "onCreate : Called 3");
         setContentView(R.layout.activity_main);
     }
 }
