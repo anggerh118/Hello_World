3237d375680fb8f061e951068bf1ade3c1099409
ASUSTUF505GD\ASUS <angger.herlambang@ti.ukdw.ac.id>
2020-10-15 17:25:38 +0700

    commit LoginBranch

--- app/src/main/java/com/example/helloworld/HomeActivity2.java
+++ app/src/main/java/com/example/helloworld/HomeActivity2.java
@@ -1,8 +1,12 @@
 package com.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
 
 public class HomeActivity2 extends AppCompatActivity {
 
@@ -10,5 +14,37 @@ public class HomeActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
+        hideTitleBar();
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+        TabItem layoutOne = findViewById(R.id.Fragment1);
+        TabItem layoutTwo = findViewById(R.id.Fragment2);
+        final ViewPager viewPager = findViewById(R.id.viewPager);
+
+        PagerAdapter pagerAdapter = new PagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount() );
+        viewPager.setAdapter(pagerAdapter);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+    }
+
+    private void hideTitleBar(){
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
     }
 }
